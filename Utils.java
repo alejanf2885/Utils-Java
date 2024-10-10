fec4e84363b8ba106c094e6319dacfec1253a68e
Alejandro Navarro Fernández <146034573+alejanf2885@users.noreply.github.com>
2024-10-10 20:20:36 +0200

    Cifrado AES Java
    Ejemplo de cifrado AES en Java para poder implementarse, utiliza AES en modo GCM.

---
+++ Utils.java
@@ -0,0 +1,43 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class Utils {
+
+    public static SecretKey generateKey(int n) throws NoSuchAlgorithmException {
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(n);
+        return keyGen.generateKey();
+    }
+
+    public static String encrypt(String data, SecretKey key) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        byte[] iv = new byte[16];
+        SecureRandom random = new SecureRandom();
+        random.nextBytes(iv);
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+        byte[] encryptedData = cipher.doFinal(data.getBytes());
+        byte[] encryptedDataWithIv = new byte[iv.length + encryptedData.length];
+        System.arraycopy(iv, 0, encryptedDataWithIv, 0, iv.length);
+        System.arraycopy(encryptedData, 0, encryptedDataWithIv, iv.length, encryptedData.length);
+        return Base64.getEncoder().encodeToString(encryptedDataWithIv);
+    }
+
+    public static String decrypt(String encryptedData, SecretKey key) throws Exception {
+        byte[] encryptedDataWithIv = Base64.getDecoder().decode(encryptedData);
+        byte[] iv = new byte[16];
+        byte[] encryptedBytes = new byte[encryptedDataWithIv.length - iv.length];
+        System.arraycopy(encryptedDataWithIv, 0, iv, 0, iv.length);
+        System.arraycopy(encryptedDataWithIv, iv.length, encryptedBytes, 0, encryptedBytes.length);
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.DECRYPT_MODE, key, ivSpec);
+        byte[] decryptedData = cipher.doFinal(encryptedBytes);
+        return new String(decryptedData);
+    }
+}
